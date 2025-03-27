86893fb76e3e6ccff9b06f9ef3c59e911e8e315e
zagraevskaya2025 <zagraevskaya@yandex.ru>
2025-03-27 22:21:20 +0300

    Update urls.py

--- spa_hotel/urls.py
+++ spa_hotel/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
     path('accounts/logout/', logout_view, name='logout'),  #  кастомный выход
     path('accounts/', include('django.contrib.auth.urls')),  # остальное из auth
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
